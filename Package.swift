2b5e657bc1b6559629f0bcaa0d7f450d101dd1bf
kobayashi daiki <nissy_0924@icloud.com>
2025-10-04 23:54:34 +0900

    XCFrameworkの配布

--- Package.swift
+++ Package.swift
@@ -7,15 +7,14 @@ let package = Package(
     name: "BarcodeScanSdkIos",
     products: [
         .library(
-            name: "BarcodeScanSdkIos",
-            targets: ["BarcodeScanSdkIos"]),
+            name: "BarcodeScanSdk",
+            targets: ["BarcodeScanSdk"]),
     ],
     targets: [
-        .target(
-            name: "BarcodeScanSdkIos"),
-        .testTarget(
-            name: "BarcodeScanSdkIosTests",
-            dependencies: ["BarcodeScanSdkIos"]
-        ),
+        .binaryTarget(
+            name: "BarcodeScanSdk",
+            url: "https://github.com/daiki0924/barcode_scan_sdk_ios/releases/download/v0.1.0/BarcodeScan.xcframework.zip",
+            checksum: "56391536df203314a8998385a9e7cf3f23362f5cfebaf81e2145f102e3280f80"
+        )
     ]
 )
